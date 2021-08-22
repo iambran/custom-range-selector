319c79fc8054b3f3598a859f7cf39eaac6ff915b
bran <kangaroodesign@Kangaroos-MacBook-Air.local>
2021-08-22 23:16:46 +0800

    add basic codes

---
+++ main.js
@@ -0,0 +1,33 @@
+// https://github.com/bradtraversy/50projects50days
+
+const range = document.querySelector('#range');
+
+range.addEventListener('input', (e) => {
+    const value = +e.target.value;
+    const label = e.target.nextElementSibling;
+
+    const range_width = getComputedStyle(e.target).getPropertyValue('width');
+    console.log(range_width);
+    const label_width = getComputedStyle(label).getPropertyValue('width');
+    console.log(label_width);
+
+
+    // like '300px' get converted to '300', and to number with '+' at the beginner
+    const num_range_width = +range_width.substring(0, range_width.length - 2);
+    const num_label_width = +label_width.substring(0, label_width.length - 2);
+
+    const max = +e.target.max;
+    const min = +e.target.min;
+
+    const left = value * (num_range_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10);
+    console.log(left);
+
+    label.style.left = `${left}px`;
+    label.innerHTML = value;
+
+})
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
